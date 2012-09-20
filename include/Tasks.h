151f42ed1156f2b6d361aa08c0214c5f2d15bf65
Priit Kallas <kallaspriit@gmail.com>
2012-09-20 13:40:53 +0300

    Started tasks

---
+++ include/Tasks.h
@@ -0,0 +1,42 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+#include "Math.h"
+
+#include <string>
+
+class Robot;
+
+class Task {
+    public:
+        Task() : _ready(false) {}
+        virtual void onStart(Robot& robot, double dt) {};
+        virtual bool onStep(Robot& robot, double dt) { return false; };
+        virtual void onEnd(Robot& robot, double dt) {};
+        virtual std::string toString() { return "[Task]"; }
+
+        bool _ready;
+
+    private:
+};
+
+class TurnByTask : public Task {
+    public:
+        TurnByTask(float angle, float speed = 1) : Task(), threshold(Math::PI / 30.0f), speed(speed), turnAngle(angle) {}
+
+        void onStart(Robot& robot, double dt);
+        bool onStep(Robot& robot, double dt);
+        void onEnd(Robot& robot, double dt);
+        std::string toString();
+
+    private:
+        const float threshold;
+        float speed;
+        float startAngle;
+        float turnAngle;
+        float targetAngle;
+        float currentAngle;
+        float diff;
+};
+
+#endif // TASKS_H
---
+++ src/Tasks.cpp
@@ -0,0 +1,36 @@
+#include "Tasks.h"
+#include "Robot.h"
+#include "Util.h"
+
+#include <iostream>
+
+// Turn by angle task
+void TurnByTask::onStart(Robot& robot, double dt) {
+    startAngle = robot.getOrientation();
+}
+
+bool TurnByTask::onStep(Robot& robot, double dt) {
+    targetAngle = Math::floatModulus(startAngle + turnAngle, Math::TWO_PI);
+    currentAngle = robot.getOrientation();
+    diff = Util::abs(currentAngle - targetAngle);
+
+    if (diff < threshold) {
+        return false;
+    }
+
+    float dir = targetAngle > currentAngle ? 1.0f : -1.0f;
+
+    robot.setTargetOmega(dir * speed);
+
+    return true;
+}
+
+void TurnByTask::onEnd(Robot& robot, double dt) {
+    robot.setTargetOmega(0);
+}
+
+std::string TurnByTask::toString() {
+    float percent = 50.0f;
+
+    return "TurnBy " + Util::toString(Util::round(Math::radToDeg(turnAngle), 1)) + "deg - " + Util::toString(percent) + "%";
+}
