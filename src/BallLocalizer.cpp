aa14b665226b83d4f74a92469b99adea4c5566e1
Priit Kallas <kallaspriit@gmail.com>
2012-09-23 22:24:20 +0300

    Updated

--- src/BallLocalizer.cpp
+++ src/BallLocalizer.cpp
@@ -9,7 +9,7 @@ BallLocalizer::~BallLocalizer() {
 }
 
 void BallLocalizer::update(Math::Position robotPosition, const BallList& visibleBalls, const Math::Polygon& cameraFOV, double dt) {
-    for (int i = 0; i < visibleBalls.size(); i++) {
+    for (unsigned int i = 0; i < visibleBalls.size(); i++) {
         float angle = Math::floatModulus(robotPosition.orientation + visibleBalls[i]->angle, Math::TWO_PI);
 
         visibleBalls[i]->x = robotPosition.x + Math::cos(angle) * visibleBalls[i]->distance;
