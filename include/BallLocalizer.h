7fe0ae39edd7fd8cf0f9db4d3f9d536e885823d7
Priit Kallas <kallaspriit@gmail.com>
2012-09-23 20:18:37 +0300

    Updated

--- include/BallLocalizer.h
+++ include/BallLocalizer.h
@@ -1,33 +1,6 @@
 #ifndef BALLLOCALIZER_H
 #define BALLLOCALIZER_H
 
-class Ball {
-    public:
-        Ball(float x, float y, float distance, float angle, double dt);
-        void updateVisible(float x, float y, float distance, float angle, double dt);
-        void updateInvisible(double dt);
-        void markForRemoval(double afterSeconds);
-        bool shouldBeRemoved();
-
-        int id;
-        double createdTime;
-        double updatedTime;
-        double removeTime;
-        float x;
-        float y;
-        float velocityX;
-        float velocityY;
-        float distance;
-        float angle;
-        float elasticity;
-        float radius;
-        bool visible;
-    private:
-        static int instances;
-
-        void applyDrag(double dt);
-};
-
 class BallLocalizer {
     public:
         BallLocalizer();
