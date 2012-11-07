34fefd6427b014a083cdf8417528326c8d3eee37
Priit Kallas <kallaspriit@gmail.com>
2012-11-07 17:23:05 +0200

    Updated

--- dash/soccerbot.php
+++ dash/soccerbot.php
@@ -1,10 +1,18 @@
 <?php
 
+function _exec($cmd) {
+	$shell = new COM("WScript.Shell");
+	$result = $shell->Run($cmd, 0, false);
+	echo $cmd;
+	return $result == 0 ? true : false;
+}
+
 function rebuild() {
 	echo exec('call "C:/soccerbot/kill.bat"')."\n";
 	echo exec('call "C:/soccerbot/update.bat"')."\n";
 	echo exec('call "C:/soccerbot/build.bat"')."\n";
-	echo exec('call "C:/soccerbot/run.bat"')."\n";
+	_exec('C:/soccerbot/soccerbot.exe');
+	//echo exec('call "C:/soccerbot/run.bat"')."\n";
 	//echo exec('start /B "Soccerbot" C:/soccerbot/soccerbot.exe')."\n";
 	//pclose(popen('start "bla" "C:/soccerbot/soccerbot.exe"', "r"));
 }
