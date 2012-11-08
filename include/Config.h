c5f6c00e9f4d20a66c25128ccdc51a71f9a37016
Priit Kallas <kallaspriit@gmail.com>
2012-11-08 22:40:42 +0200

    Updated

--- include/Config.h
+++ include/Config.h
@@ -74,7 +74,7 @@ const int validGoalMinMatches = 10;
 const int whiteBlackMinY = 100;
 const int maxBallSenseRadius = 100;
 const float rotationStoppedOmegaThreshold = 0.25f;
-const float rotationCancelMultiplier = 1.0f;
+const float rotationCancelMultiplier = 0.5f;
 
 } // namespace Config
 
