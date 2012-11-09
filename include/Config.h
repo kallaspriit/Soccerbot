d31ccbeeb42f9724adc98323c152a604cf89894f
Priit Kallas <kallaspriit@gmail.com>
2012-11-09 18:40:39 +0200

    Updated

--- include/Config.h
+++ include/Config.h
@@ -83,7 +83,7 @@ const float ballChaseAngleMaxSlowdown = 0.9f;
 //const float chaseBallBrakeMultiplier = 20.0f;
 const float chaseBallBrakeMultiplier = 0.5f;
 const float chaseBallMaxBrakeSpeed = 2.0f;
-const float brakeDistanceMultiplier = 1.5f;
+const float brakeDistanceMultiplier = 1.0f;
 const float distanceCorrection = -0.03f;
 const int maxSurroundSenseY = cameraHeight / 2 - 50;
 
