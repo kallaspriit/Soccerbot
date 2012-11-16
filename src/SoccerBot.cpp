57cf1beb424b43c3ea506ac863005a0764e991b0
Priit Kallas <kallaspriit@gmail.com>
2012-11-16 19:06:42 +0200

    Updated

--- src/SoccerBot.cpp
+++ src/SoccerBot.cpp
@@ -264,7 +264,7 @@ void SoccerBot::setupCameras() {
 		//frontCamera = NULL;
 	}
 
-	/*if (rearCamera->open(Config::rearCameraSerial)) {
+	if (rearCamera->open(Config::rearCameraSerial)) {
         configureCamera(rearCamera);
 		showCameraInfo(rearCamera, "! Rear camera");
 
@@ -277,7 +277,7 @@ void SoccerBot::setupCameras() {
 		//delete rearCamera;
 
 		//rearCamera = NULL;
-	}*/
+	}
 
 	std::cout << "! Cameras ready" << std::endl;
 }
