d1a8d68f18f8c27fdf9d9fc6032f30b7f9b6b32e
Priit Kallas <kallaspriit@gmail.com>
2012-11-18 23:34:50 +0200

    Updated

--- src/SimpleAI.cpp
+++ src/SimpleAI.cpp
@@ -365,8 +365,6 @@ void SimpleAI::stepFetchBall(double dt) {
 				if (currentVelocityX > Config::ballChaseNearSpeed && currentVelocityX < lastVelocityX) {
 					speed = -Math::max(Config::chaseBallBrakeMultiplier * currentVelocityX, Config::chaseBallMaxBrakeSpeed);
 					braking = true;
-
-					std::cout << "@ BRAKING SPEED: " << speed << std::endl;
 				} else {
 					nearSpeedReached = true;
 				}
