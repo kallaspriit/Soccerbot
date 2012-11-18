1f2a2014066bc9425e8c56dcb8fc3a5c0f4e7cbf
Priit Kallas <kallaspriit@gmail.com>
2012-11-18 18:19:48 +0200

    Updated

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -276,7 +276,7 @@ void Robot::setPosition(float x, float y, float orientation) {
 }
 
 void Robot::updateGyroOrientation(float deltaAngle) {
-	//gyroOrientation = Math::floatModulus(gyroOrientation + deltaAngle, Math::TWO_PI);
+	gyroOrientation = Math::floatModulus(gyroOrientation + deltaAngle, Math::TWO_PI);
 	orientation = Math::floatModulus(orientation + deltaAngle, Math::TWO_PI);
 }
 
