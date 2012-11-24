0f67e0e58042b0b3ddd655e8efb6593415f9bb46
Priit Kallas <kallaspriit@gmail.com>
2012-11-24 03:09:41 +0200

    Updated

--- include/Config.h
+++ include/Config.h
@@ -86,7 +86,7 @@ const float ballFocusP = 5.0f;						//< how fast to yaw to focus on a ball (angl
 const float ballFocusI = 0.0f;						//< how fast to yaw to focus on a ball (angle multiplier)
 const float ballFocusD = 0.0f;						//< how fast to yaw to focus on a ball (angle multiplier)
 
-const float goalFocusP = 5.0f;						//< how fast to yaw to focus on a goal (angle multiplier)
+const float goalFocusP = 4.0f;						//< how fast to yaw to focus on a goal (angle multiplier)
 const float ballSearchOmega = 3.14f;				//< how fast to spin to search for ball
 const float ballRotateOmega = 2.0f;					//< how fast to rotate while searching for goal
 const float goalSpinOmega = 1.5f;					//< how fast to rotate while searching for goal
--- src/SimpleAI.cpp
+++ src/SimpleAI.cpp
@@ -565,11 +565,12 @@ void SimpleAI::stepFindGoal(double dt) {
 			if (goal->behind) {
 				robot->spinAroundDribbler(goal->angle < 0.0f ? true : false);
 			} else {
-				float period = Math::limit(Config::goalAimPeriod / Math::abs(goal->angle), 2.0, 10.0);
+				//float period = Math::limit(Config::goalAimPeriod / Math::abs(goal->angle), 2.0, 10.0);
+				float period = Math::limit(Config::goalAimPeriod / Math::abs(goal->angle), 2.0, 20.0);
 
-				//robot->spinAroundDribbler(goal->angle < 0.0f ? true : false, period, Config::spinAroundDribblerRadius, 0.0f);
+				robot->spinAroundDribbler(goal->angle < 0.0f ? true : false, period, Config::spinAroundDribblerRadius, 0.0f);
 			
-				robot->setTargetDir(Math::Rad(0), speed, omega);
+				//robot->setTargetDir(Math::Rad(0), speed, omega);
 			}
 		} else {
 			if (goal->behind) {
