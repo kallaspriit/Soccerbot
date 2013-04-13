8708e29c7b582cc6cb7e2bb00015c6230531521f
Priit Kallas <kallaspriit@gmail.com>
2013-04-13 14:35:42 +0300

    ...

--- src/Object.cpp
+++ src/Object.cpp
@@ -30,10 +30,10 @@ bool Object::intersects(Object* other, int margin) {
 	int by1 = other->y - other->height / 2 - margin;
 	int by2 = other->y + other->height / 2 + margin;
 
-	return !(ax1 > bx2 || ax2 < bx1 || ay1 < by2 || ay2 > by1);
+	/*return !(ax1 > bx2 || ax2 < bx1 || ay1 < by2 || ay2 > by1);*/
   /*return !(bx1 > ax2 || bx2 < ax1 || by2 < ay1 || by1 > ay2);*/
 
-	if (
+	/*if (
 		ax1 >= bx2 
 		|| ax2 <= bx1 
 		|| ay1 <= by2
@@ -42,17 +42,16 @@ bool Object::intersects(Object* other, int margin) {
 		  return false;
 	}
     
-	return true;
+	return true;*/
 
 	/*separate = rect_a.right < rect_b.left or
         rect_a.left > rect_b.right or
         rect_a.top < rect_b.bottom or
         rect_a.bottom > rect_b.top*/
 
-	/*separate = ax2 < bx1 or
-        ax1 > bx2 or
-        ay1 < by2 or
-        ay2 > by1*/
+	bool separate = ax2 < bx1 || ax1 > bx2 || ay1 < by2 || ay2 > by1;
+
+	return !separate;
 
 	//return ax1 < bx2 && ax2 > bx1 && ay1 < by2 && ay2 > by1;
 	
