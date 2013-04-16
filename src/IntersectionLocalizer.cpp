05f027d0a6543cec16289035c47f20800a717afd
Priit Kallas <kallaspriit@gmail.com>
2013-04-16 19:47:14 +0300

    ...

--- src/IntersectionLocalizer.cpp
+++ src/IntersectionLocalizer.cpp
@@ -35,36 +35,132 @@ void IntersectionLocalizer::move(float velocityX, float velocityY, float omega, 
 }
 
 void IntersectionLocalizer::update(float yellowDistance, float blueDistance, float yellowAngle, float blueAngle, Side frontGoal) {
-	if (blueDistance <= 0.0f || yellowDistance <= 0.0f) {
+	if (blueDistance == 0.0f || yellowDistance == 0.0f) {
 		std::cout << "- Invalid goal distance: " << blueDistance << ", " << yellowDistance << std::endl;
 
 		return;
 	}
+
+	bool intersectionsFound = false;
 	
-	Math::Circle yellowCircle = Math::Circle(yellowGoalPos.x, yellowGoalPos.y, yellowDistance);
-    Math::Circle blueCircle = Math::Circle(blueGoalPos.x, blueGoalPos.y, blueDistance);
-    Math::Circle::Intersections intersections = yellowCircle.getIntersections(blueCircle);
+	if (yellowDistance != -1.0f && blueDistance != -1.0f) {
+		Math::Circle yellowCircle = Math::Circle(yellowGoalPos.x, yellowGoalPos.y, yellowDistance);
+		Math::Circle blueCircle = Math::Circle(blueGoalPos.x, blueGoalPos.y, blueDistance);
+		Math::Circle::Intersections intersections = yellowCircle.getIntersections(blueCircle);
 
-    if (!intersections.exist) {
-		Math::Vector currentPos(x, y), dirVector, scaledDir, newPos;
+		if (intersections.exist) {
+			intersectionsFound = true;
+			std::string correctIntersection = "unsure";
 
-		if (yellowDistance != -1.0f && currentPos.distanceTo(yellowGoalPos) != 0) {
-			dirVector = Math::Vector::createDirVec(currentPos, yellowGoalPos);
-			scaledDir = dirVector.getScaled(yellowDistance);
-			newPos = dirVector.getSummed(yellowGoalPos);
+			if (blueAngle > 0 && yellowAngle > 0) {
+				correctIntersection = frontGoal == Side::BLUE ? "top" : "bottom";
+			} else if (blueAngle < 0 && yellowAngle < 0) {
+				correctIntersection = frontGoal == Side::BLUE ? "bottom" : "top";
+			} else {
+				float distance1 = Math::distanceBetween(
+					intersections.x1, intersections.y1,
+					x, y
+				);
+				float distance2 = Math::distanceBetween(
+					intersections.x2, intersections.y2,
+					x, y
+				);
 
-			x = newPos.x;
-			y = newPos.y;
-	    } else if (blueDistance != -1.0f && currentPos.distanceTo(blueGoalPos) != 0) {
-			dirVector = Math::Vector::createDirVec(currentPos, blueGoalPos);
-			scaledDir = dirVector.getScaled(blueDistance);
-			newPos = dirVector.getSummed(blueGoalPos);
+				if (distance1 < distance2) {
+					x = intersections.x1;
+					y = intersections.y1;
+				} else {
+					x = intersections.x2;
+					y = intersections.y2;
+				}
+			}
 
-			x = newPos.x;
-			y = newPos.y;
-	    }
+			if (correctIntersection == "top") {
+				x = intersections.x2;
+				y = intersections.y2;
+			} else if (correctIntersection == "bottom") {
+				x = intersections.x1;
+				y = intersections.y1;
+			}
 
-		Util::confineField(x, y);
+			Util::confineField(x, y);
+
+			float verticalOffset = y - Config::fieldHeight / 2.0;
+			float zeroAngleBlue = Math::asin(verticalOffset / blueDistance);
+			float zeroAngleYellow = Math::asin(verticalOffset / yellowDistance);
+			float posYellowAngle = yellowAngle < 0 ? yellowAngle + Math::TWO_PI : yellowAngle;
+			float posBlueAngle = blueAngle < 0 ? blueAngle + Math::TWO_PI : blueAngle;
+			float yellowGuess = Math::floatModulus(Math::PI - (posYellowAngle - zeroAngleYellow), Math::TWO_PI);
+			float blueGuess = Math::floatModulus(-zeroAngleBlue - posBlueAngle, Math::TWO_PI);
+
+			while (yellowGuess < 0) {
+				yellowGuess += Math::TWO_PI;
+			}
+
+			while (blueGuess < 0) {
+				blueGuess += Math::TWO_PI;
+			}
+
+			orientation = Math::getAngleAvg(yellowGuess, blueGuess);
+
+			if (orientation < 0) {
+				orientation += Math::TWO_PI;
+			}
+
+			std::stringstream stream;
+
+			stream << "{";
+			stream << "\"intersections\": true,";
+			stream << "\"yellowDistance\": " << yellowDistance << ",";
+			stream << "\"blueDistance\": " << blueDistance << ",";
+			stream << "\"correctIntersection\": \"" << correctIntersection << "\",";
+			stream << "\"x\": " << x << ",";
+			stream << "\"y\": " << y << ",";
+			stream << "\"orientation\": " << orientation;
+			stream << "}";
+
+			json = stream.str();
+
+			return;
+		}
+	}
+
+	if (!intersectionsFound) {
+		if (yellowDistance != -1.0f || blueDistance != -1.0f) {
+			Math::Vector currentPos(x, y), dirVector, scaledDir, newPos;
+
+			Side closestVisible = Side::UNKNOWN;
+
+			if (yellowDistance != -1 && blueDistance != -1) {
+				if (yellowDistance < blueDistance) {
+					closestVisible = Side::YELLOW;
+				} else {
+					closestVisible = Side::BLUE;
+				}
+			} else if (yellowDistance != -1) {
+				closestVisible = Side::YELLOW;
+			} else {
+				closestVisible = Side::BLUE;
+			}
+
+			if (closestVisible == Side::YELLOW && currentPos.distanceTo(yellowGoalPos) != 0) {
+				dirVector = Math::Vector::createDirVec(currentPos, yellowGoalPos);
+				scaledDir = dirVector.getScaled(yellowDistance);
+				newPos = dirVector.getSummed(yellowGoalPos);
+
+				x = newPos.x;
+				y = newPos.y;
+			} else if (closestVisible == Side::BLUE && currentPos.distanceTo(blueGoalPos) != 0) {
+				dirVector = Math::Vector::createDirVec(currentPos, blueGoalPos);
+				scaledDir = dirVector.getScaled(blueDistance);
+				newPos = dirVector.getSummed(blueGoalPos);
+
+				x = newPos.x;
+				y = newPos.y;
+			}
+
+			Util::confineField(x, y);
+		}
 
 		std::stringstream stream;
 
@@ -79,78 +175,6 @@ void IntersectionLocalizer::update(float yellowDistance, float blueDistance, flo
 		stream << "}";
 
 		json = stream.str();
-
-        return;
-    }
-
-	std::string correctIntersection = "unsure";
-
-	if (blueAngle > 0 && yellowAngle > 0) {
-		correctIntersection = frontGoal == Side::BLUE ? "top" : "bottom";
-	} else if (blueAngle < 0 && yellowAngle < 0) {
-		correctIntersection = frontGoal == Side::BLUE ? "bottom" : "top";
-	} else {
-		float distance1 = Math::distanceBetween(
-			intersections.x1, intersections.y1,
-			x, y
-		);
-		float distance2 = Math::distanceBetween(
-			intersections.x2, intersections.y2,
-			x, y
-		);
-
-		if (distance1 < distance2) {
-			x = intersections.x1;
-			y = intersections.y1;
-		} else {
-			x = intersections.x2;
-			y = intersections.y2;
-		}
 	}
-
-	if (correctIntersection == "top") {
-		x = intersections.x2;
-		y = intersections.y2;
-	} else if (correctIntersection == "bottom") {
-		x = intersections.x1;
-		y = intersections.y1;
-	}
-
-	Util::confineField(x, y);
-
-	float verticalOffset = y - Config::fieldHeight / 2.0;
-	float zeroAngleBlue = Math::asin(verticalOffset / blueDistance);
-	float zeroAngleYellow = Math::asin(verticalOffset / yellowDistance);
-	float posYellowAngle = yellowAngle < 0 ? yellowAngle + Math::TWO_PI : yellowAngle;
-	float posBlueAngle = blueAngle < 0 ? blueAngle + Math::TWO_PI : blueAngle;
-	float yellowGuess = Math::floatModulus(Math::PI - (posYellowAngle - zeroAngleYellow), Math::TWO_PI);
-	float blueGuess = Math::floatModulus(-zeroAngleBlue - posBlueAngle, Math::TWO_PI);
-
-	while (yellowGuess < 0) {
-		yellowGuess += Math::TWO_PI;
-	}
-
-	while (blueGuess < 0) {
-		blueGuess += Math::TWO_PI;
-	}
-
-	orientation = Math::getAngleAvg(yellowGuess, blueGuess);
-
-	if (orientation < 0) {
-		orientation += Math::TWO_PI;
-	}
-
-	std::stringstream stream;
-
-    stream << "{";
-	stream << "\"intersections\": true,";
-	stream << "\"yellowDistance\": " << yellowDistance << ",";
-	stream << "\"blueDistance\": " << blueDistance << ",";
-	stream << "\"correctIntersection\": \"" << correctIntersection << "\",";
-	stream << "\"x\": " << x << ",";
-	stream << "\"y\": " << y << ",";
-	stream << "\"orientation\": " << orientation;
-	stream << "}";
-
-    json = stream.str();
+	
 }
