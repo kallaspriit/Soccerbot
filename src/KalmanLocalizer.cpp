f9f686eff0958efd5ad0edfea59953b20aeac215
Priit Kallas <kallaspriit@gmail.com>
2013-04-20 15:44:37 +0300

    ...

--- src/KalmanLocalizer.cpp
+++ src/KalmanLocalizer.cpp
@@ -16,8 +16,8 @@ KalmanLocalizer::KalmanLocalizer() : filter(NULL) {
 
 	double velocityPreserve = 0.5;
 	double covariance = 0.1;
-	double processError = 0.000001;
-	double measurementError = 0.5;
+	double processError = 0.0000001;
+	double measurementError = 0.25;
 
 	stateTransitionMatrix
 		<< 1.00 << 0.00 << 1.00 << 0.00 << 0.00 << arma::endr				// x
